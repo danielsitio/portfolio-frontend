c6be27ccd84dff456e1304f552ccbc70eaa48e6f
danielsitio <danest.esc@gmail.com>
2023-04-08 23:48:22 -0300

    clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment'
+import { Subscription, Observable } from 'rxjs'
+import { Project } from 'src/app/shared/project';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio-frontend';
+  numero = 1
+  $respuesta: Observable<Project> | undefined
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  sendLogin() {
+    this.numero++
+    this.$respuesta = this.http.get<Project>(environment.apiUrl + "/projects")
+  }
 }
